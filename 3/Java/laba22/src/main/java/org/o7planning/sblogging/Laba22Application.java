e8fbd597b141eedf3006fcd1f16f2e6088ea97e1
lil_avokado <sanyahorban@gmail.com>
2023-11-28 22:26:17 +0200

    Laba 22

---
+++ 3/Java/laba22/src/main/java/org/o7planning/sblogging/Laba22Application.java
@@ -0,0 +1,13 @@
+package org.o7planning.sblogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laba22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laba22Application.class, args);
+	}
+
+}
---
+++ 3/Java/laba22/src/test/java/org/o7planning/sblogging/Laba22ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sblogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laba22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
