b9b23692a61956b31c7c98d322d57dd6c9bc2aca
lil_avokado <sanyahorban@gmail.com>
2023-12-06 21:26:33 +0200

    Laba 29

---
+++ 3/Java/laba29/SpringBootSecurityJPA/src/test/java/org/o7planning/sbsecurity/SpringBootSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.o7planning.sbsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
